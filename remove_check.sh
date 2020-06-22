c8eea4eb1f0022869d996368036e5187609ac8be
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-06-22 10:59:50 +0900

    Change the method of handling exceptions

--- remove_check.sh
+++ remove_check.sh
@@ -8,12 +8,12 @@ nopackage2="No package"
 
 while read line; do
         remove_message=$(yum remove -y $line 2>&1)
-        if [[ $remove_message =~ $success ]]; then
-                echo "Remove,$line,P,$(rpm -qa | wc -l)"
-        elif [[ $remove_message =~ $protected ]]; then
+        if [[ $remove_message =~ $protected ]]; then
                 echo "Remove,$line,Protected Package,$(rpm -qa | wc -l)"
         elif [[ $remove_message =~ $nopackage ]] || [[ $remove_message =~ $nopackage2 ]]; then
                 echo "Remove,$line,No Package,$(rpm -qa | wc -l)"
+        elif [[ $remove_message =~ $success ]]; then
+                echo "Remove,$line,P,$(rpm -qa | wc -l)"
         else
                 echo "Remove,$line,Exception,$(rpm -qa | wc -l)"
         fi
