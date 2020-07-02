fb85825b2ddf16fd687d0f4bc5c69e181a350fb6
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-07-02 17:50:19 +0900

    Upload version check scripts

---
+++ version_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+file=$1
+while read line; do
+	repository_version=$(repoquery --queryformat %{VERSION} $line)
+	installed_version=$(rpm -q --queryformat %{VERSION} $line)
+
+	if [[ $repository_version == $installed_version ]]; then
+		echo $line":PASS"
+	else
+		echo $line":FAIL"
+	fi
+done < $file
+
+# input : package name list (ex. acl)
+# extract only name method : rpm -qa --queryformat '%{NAME}\n' | sort > test_list
