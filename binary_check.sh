3e8a82276940c68ab138b2ebf64f34f5b4f017c6
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-07-03 17:35:53 +0900

    Update binary_check.sh
    Comment out the old code, added case-insensitive search and usage exceptions

--- binary_check.sh
+++ binary_check.sh
@@ -1,32 +1,52 @@
 #!/bin/bash
 file=$1
-bin_path="/usr/bin/"
-sbin_path="/usr/sbin/"
+#bin_path="/usr/bin/"
+#sbin_path="/usr/sbin/"
+#sbin_path2="/sbin/"
 options="OPTIONS"
+usage="Usage"
 
+#echo "help check"
 while read line; do
+        #echo $line
+        status_message=$(man $line 2>&1)
+        if [[ ${status_message,,} =~ ${options,,} ]] || [[ ${status_message,,} =~ ${usage,,} ]]; then
+                echo $line":PASS"
+        else
+                echo $line":FAIL"
+        fi
+
+:<<END
+        echo ${line#*bin/}
+        status_message=$(man ${line#*bin/} 2>&1)
+        if [[ $status_message =~ $options ]]; then
+                echo $line":PASS"
+        else
+                echo $line":FAIL"
+        fi
 
         if [[ $line =~ $bin_path ]]; then
-		#echo "bin"
-	        status_message=$(man ${line#${bin_path}} 2>&1)
-		#echo $status_message 
-        	if [[ $status_message =~ $options ]]; then
-			echo $line": help exist"
-		else
-			echo $line": help non-exsit"
-		fi	
+                #echo "bin"
+                #echo ${line#${bin_path}}
+                status_message=$(man ${line#${bin_path}} 2>&1)
+                #echo $status_message
+                if [[ $status_message =~ $options ]]; then
+                        echo $line":PASS"
+                else
+                        echo $line":FAIL"
+                fi
         elif [[ $line =~ $sbin_path ]]; then
-		#echo "sbin"
-	        status_message=$(man ${line#${sbin_path}} 2>&1)
-		#echo $status_message
-        	if [[ $status_message =~ $options ]]; then
-			echo $line": help exist"
-		else
-			echo $line": help non-exist"
-		fi	
-	else
-		echo $line"has in exception case"
-	fi 
+                #echo "sbin"
+                #echo ${line#${sbin_path}}
+                status_message=$(man ${line#${sbin_path}} 2>&1)
+                #echo $status_message
+                if [[ $status_message =~ $options ]]; then
+                        echo $line":PASS"
+                else
+                        echo $line":FAIL"
+                fi
+        else
+                echo $line":EXCEPTION"
+        fi
+END
 done < $file
-
-
