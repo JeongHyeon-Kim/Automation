e5d1a58b74f06574e413414348c59747060f7ac8
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-07-09 14:38:35 +0900

    Remove comments

--- binary_check.sh
+++ binary_check.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 file=$1
-#bin_path="/usr/bin/"
-#sbin_path="/usr/sbin/"
-#sbin_path2="/sbin/"
 options="OPTIONS"
 usage="Usage"
 
@@ -15,38 +12,4 @@ while read line; do
         else
                 echo $line":FAIL"
         fi
-
-:<<END
-        echo ${line#*bin/}
-        status_message=$(man ${line#*bin/} 2>&1)
-        if [[ $status_message =~ $options ]]; then
-                echo $line":PASS"
-        else
-                echo $line":FAIL"
-        fi
-
-        if [[ $line =~ $bin_path ]]; then
-                #echo "bin"
-                #echo ${line#${bin_path}}
-                status_message=$(man ${line#${bin_path}} 2>&1)
-                #echo $status_message
-                if [[ $status_message =~ $options ]]; then
-                        echo $line":PASS"
-                else
-                        echo $line":FAIL"
-                fi
-        elif [[ $line =~ $sbin_path ]]; then
-                #echo "sbin"
-                #echo ${line#${sbin_path}}
-                status_message=$(man ${line#${sbin_path}} 2>&1)
-                #echo $status_message
-                if [[ $status_message =~ $options ]]; then
-                        echo $line":PASS"
-                else
-                        echo $line":FAIL"
-                fi
-        else
-                echo $line":EXCEPTION"
-        fi
-END
 done < $file
