49b2318c8909e314820d7226c456e576aad19b20
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2020-09-02 16:31:28 +0900

    Completed writing scripts related of cluster configuration

--- configuration/high_availability_clustering.sh
+++ configuration/high_availability_clustering.sh
@@ -16,3 +16,33 @@ passwd hacluster
 
 echo "pcs 데몬 활성화"
 systemctl enable --now pcsd.service
+
+echo "/etc/hosts 아래 노드 추가(IP에 따라 사용자 정의 설정 가능)"
+echo "192.168.17.38 node1" >> /etc/hosts
+echo "192.168.17.123 node2" >> /etc/hosts
+
+echo "node 설정(이름: node1), 비밀 번호 입력"
+yes "asdf" | pcs host auth node1 node2 -u hacluster
+
+echo "클러스터 생성"
+pcs cluster setup pacemaker1 node1 node2
+
+echo "클러스터 모두 시작"
+pcs cluster start --all
+# systemctl start pacemaker.service
+
+echo "부팅 시 모든 클러스터 자동 시작 설정"
+pcs cluster enable --all
+# systemctl enable pacemaker.service
+
+echo "펜싱 기능 비활성화"
+pcs property set stonith-enabled=false
+
+echo "쿼럼 상태를 무시하도록 클러스터를 구성"
+pcs property set no-quorum-policy=ignore
+
+echo "마이그레이션 정책 구성(기본값)"
+pcs resource defaults migration-threshold=1
+# Warning: Defaults do not apply to resources which override them with their own defined values
+# 경고 : 기본값은 자체 정의 된 값으로 재정의하는 리소스에는 적용되지 않습니다.
+# 단일 장애 후 서비스를 새 노드로 이동하도록 클러스터가 구성
