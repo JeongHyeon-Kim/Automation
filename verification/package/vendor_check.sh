707601bf050de588d69e7315b6b863e45b70153f
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2020-09-22 20:10:13 +0900

    Improved vendor check method (copy file)

--- verification/package/vendor_check.sh
+++ verification/package/vendor_check.sh
@@ -1,12 +1,14 @@
-# 개별 패키지에 대해서 정보 얻는 방법
+# local repo에서 얻는 방법
 #!/bin/bash
 file=$1
-
 while read line; do
-        # echo $(rpm -qa --queryformat '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH};%{BUILDHOST};%{VENDOR};%{GROUP};%{SUMMARY}\n' $line)
-        echo $(rpm -qa --queryformat '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH};%{BUILDHOST};%{VENDOR};%{PACKAGER}\n' $line)
+  rpm -q --qf '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH};%{BUILDHOST};%{VENDOR};%{PACKAGER};\n' $line
 done < $file
+# 이때의 $line은 local에 마운트된 디렉토리를 list화해서 넣고 rpm 파일들을 복사한 후 그 폴더에서 진행하기
 
+# 개별 패키지에 대해서 정보 얻는 방법
+# echo $(rpm -qa --queryformat '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH};%{BUILDHOST};%{VENDOR};%{GROUP};%{SUMMARY}\n' $line)
+# echo $(rpm -qa --queryformat '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH};%{BUILDHOST};%{VENDOR};%{PACKAGER}\n' $line)
 # 이 때의 $line은 순수한 패키지 이름만 넣어주기(ex. acl(O), acl-2.2.51-14.el7.x86_64(X))
 # 이름만 추출하기 : rpm -qa --queryformat '%{NAME}\n' | sort > test_list
 # vendor_check.csv와 같은 파일로 만들고 구분자를 ;(세미콜론)으로 하기
