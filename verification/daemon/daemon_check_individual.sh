5e8953a2da1c648203ac3901688a721791c460de
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2020-10-14 18:00:47 +0900

    Deleted unnecessary content

--- verification/daemon/daemon_check_individual.sh
+++ verification/daemon/daemon_check_individual.sh
@@ -11,95 +11,95 @@ while read line; do
 	sleep 1
         status_message=$(systemctl status ${line#/usr/lib/systemd/system/} 2>&1)
         if [[ $status_message =~ $inactive ]]; then
-                echo "inactive"
+                #echo "inactive"
 		sleep 1
 		systemctl start ${line#/usr/lib/systemd/system/}
 		sleep 1
         	status_message=$(systemctl status ${line#/usr/lib/systemd/system/} 2>&1)
         	if [[ $status_message =~ $inactive ]]; then
-			echo $line",inactive start failed"
+			#echo $line",inactive start failed"
 			fail_count=$((fail_count+1))
         	elif [[ $status_message =~ $active ]]; then
-			echo $line",inactive start success"
+			#echo $line",inactive start success"
 			success_count=$((success_count+1))
 			sleep 1
 			systemctl stop ${line#/usr/lib/systemd/system/}
 			sleep 1
 	        	status_message=$(systemctl status ${line#/usr/lib/systemd/system/} 2>&1)
 			if [[ $status_message =~ $inactive ]]; then
-				echo "$line,active stop success"
+				#echo "$line,active stop success"
 				success_count=$((success_count+1))
 				sleep 1
 				systemctl start ${line#/usr/lib/systemd/system/}
 				sleep 1
 		        	status_message=$(systemctl status ${line#/usr/lib/systemd/system/} 2>&1)
         			if [[ $status_message =~ $inactive ]]; then
-					echo $line",inactive start failed"
+					#echo $line",inactive start failed"
 					fail_count=$((fail_count+1))
         			elif [[ $status_message =~ $active ]]; then
-					echo $line",inactive start success"
+					#echo $line",inactive start success"
 					success_count=$((success_count+1))
 				else
-					echo $line",Exception"
+					#echo $line",Exception"
 					exception_count=$((exception_count+1))
 				fi
 	        	elif [[ $status_message =~ $active ]]; then
-				echo $line",active stop failed"
+				#echo $line",active stop failed"
 				fail_count=$((fail_count+1))
 			else
-				echo $line",Exception"
+				#echo $line",Exception"
 				exception_count=$((exception_count+1))
 			fi
 		else
-			echo $line",Exception"
+			#echo $line",Exception"
 			exception_count=$((exception_count+1))
 		fi
         elif [[ $status_message =~ $active ]]; then
-		echo "active"
+		#echo "active"
 		sleep 1
 		systemctl stop ${line#/usr/lib/systemd/system/}
 		sleep 1
         	status_message=$(systemctl status ${line#/usr/lib/systemd/system/} 2>&1)
         	if [[ $status_message =~ $inactive ]]; then
-			echo "$line,active stop success"
+			#echo "$line,active stop success"
 			success_count=$((success_count+1))
 			sleep 1
 			systemctl start ${line#/usr/lib/systemd/system/}
 			sleep 1
 			status_message=$(systemctl status ${line#/usr/lib/systemd/system/} 2>&1)
         		if [[ $status_message =~ $inactive ]]; then
-                		echo $line",inactive start failed"
+                		#echo $line",inactive start failed"
 				fail_count=$((fail_count+1))
         		elif [[ $status_message =~ $active ]]; then
-                		echo $line",inactive start success"
+                		#echo $line",inactive start success"
 				success_count=$((success_count+1))
 				sleep 1
 				systemctl stop ${line#/usr/lib/systemd/system/}
 				sleep 1
 				status_message=$(systemctl status ${line#/usr/lib/systemd/system/} 2>&1)
 				if [[ $status_message =~ $active ]]; then
-					echo $line",active stop failed"
+					#echo $line",active stop failed"
 					fail_count=$((fail_count+1))
 				elif [[ $status_message =~ $inactive ]]; then
-					echo $line",active stop success"
+					#echo $line",active stop success"
 					success_count=$((success_count+1))
 				else
-					echo $line",Exception"
+					#echo $line",Exception"
 					exception_count=$((exception_count+1))
 				fi
         		else
-                		echo $line",Exception"
+                		#echo $line",Exception"
 				exception_count=$((exception_count+1))
         		fi
         	elif [[ $status_message =~ $active ]]; then
-			echo $line",active stop failed"
+			#echo $line",active stop failed"
 			fail_count=$((fail_count+1))
 		else
-			echo $line",Exception"
+			#echo $line",Exception"
 			exception_count=$((exception_count+1))
 		fi
 	else
-		echo $line",Exception"
+		#echo $line",Exception"
 		exception_count=$((exception_count+1))
 	fi
 	if [ $success_count -eq $pass_num ]; then
