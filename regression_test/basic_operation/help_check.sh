04a4e56189d15aa00883fdd3515db3b14d55f5dd
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2020-12-01 10:53:48 +0900

    Improved code
    - Optimized by removing the part that replaces the output with lowercase 
    letters
    - Changed separator (: -> ,)

--- regression_test/basic_operation/help_check.sh
+++ regression_test/basic_operation/help_check.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 file=$1
-options="OPTIONS"
-usage="Usage"
+options="options"
+usage="usage"
 
-#echo "help check"
 while read line; do
-        #echo $line
-        status_message=$(man $line 2>&1)
-        if [[ ${status_message,,} =~ ${options,,} ]] || [[ ${status_message,,} =~ ${usage,,} ]]; then
-                echo $line":PASS"
-        else
-                echo $line":FAIL"
-        fi
+  status_message=$(man $line 2>&1)
+  if [[ ${status_message,,} =~ $options ]] || [[ ${status_message,,} =~ $usage ]]; then
+    echo $line",PASS"
+  else
+    echo $line",FAIL"
+  fi
 done < $file
